7ed497afdb3d4a446c662aace0a1874b75df4b9b
trinhngoc1612 <58604220+trinhngoc1612@users.noreply.github.com>
2019-12-18 11:57:38 +0700

    Update 1

--- javascript.js
+++ javascript.js
@@ -1,23 +1,10 @@
-$(document).ready(function($) {
+$(document).ready(function() {
 	$("#button-menu").mouseover(function(){
-		$(".sidebar").addClass('display-none');
-		$(".more-choose").addClass('fixMore-choose');
+		$(".wrapper-more").show();
+		$(".col-menubar").hide();
 	});
-	$(".more-choose").mouseout(function(){
-		$(".sidebar").removeClass('display-none');
-		$(".more-choose").removeClass('fixMore-choose');
+	$(".wrapper-more").mouseleave(function(){
+		$(".col-menubar").show();
+		$(".wrapper-more").hide();
 	});
-	
-	
 });
-// $(window).resize(function(){
-// 	  var width = $(window).width();
-// 	  if (width<992) {
-// 			$(".wrapper-similar").addClass('row');
-// 			$(".wrapper-similar .wrapper-sidebar").addClass('col-4');
-// 		}
-// 		else {
-// 			$(".wrapper-similar").removeClass('row');
-// 			$(".wrapper-similar .wrapper-sidebar").removeClass('col-4');
-// 		}
-// 	});
