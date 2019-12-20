7e06d001f63f26d48dec406643196aca085866b8
trinhngoc1612 <58604220+trinhngoc1612@users.noreply.github.com>
2019-12-20 16:53:47 +0700

    upload2

--- javascript.js
+++ javascript.js
@@ -1,10 +1,55 @@
 $(document).ready(function() {
 	$("#button-menu").mouseover(function(){
-		$(".wrapper-more").show();
+		$(".wrapper-more").addClass("show-wrapper-more");
+		$(".div-wrapper").show();
 		$(".col-menubar").hide();
 	});
-	$(".wrapper-more").mouseleave(function(){
+	$(".wrapper-more, .sidebar").mouseleave(function(){
+		$(".div-wrapper").hide();
 		$(".col-menubar").show();
-		$(".wrapper-more").hide();
+		$(".wrapper-more").removeClass("show-wrapper-more");
+	});
+
+	$(".choose-1").mouseover(function(){
+		$(".choose-1 .title-apps").css('background-color', '#ed3b3b');
+	});
+	$(".choose-1").mouseleave(function(){
+		$(".choose-1 .title-apps").css('background-color', '#fff');
+	});
+	$(".choose-2").mouseover(function(){
+		$(".choose-2 .title-apps").css('background-color', '#039be5');
+	});
+	$(".choose-2").mouseleave(function(){
+		$(".choose-2 .title-apps").css('background-color', '#fff');
+	});
+	$(".choose-3").mouseover(function(){
+		$(".choose-3 .title-apps").css('background-color', '#444');
+	});
+	$(".choose-3").mouseleave(function(){
+		$(".choose-3 .title-apps").css('background-color', '#fff');
+	});
+
+	$(".menu-grid").click(function() {
+		$(".sub-menu-grid").toggle();
+	});
+
+	$(".btn-question").click(function(){
+		$(".wrapper-help").toggle();
+	});
+
+	$("#close-help").click(function(){
+		$(".wrapper-help").hide();
+	});
+
+	$(".what-new .collapse-change-1").click(function() {
+		$(".what-new ul").css('height', 'unset');
+		$(".what-new .collapse-change-2").show();
+		$(".what-new .collapse-change-1").hide();
+	});
+	$(".what-new .collapse-change-2").click(function() {
+		$(".what-new ul").css('height', '200px');
+		$(".what-new .collapse-change-1").show();
+		$(".what-new .collapse-change-2").hide();
+		
 	});
 });
